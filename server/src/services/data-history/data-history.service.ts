690b88d420c03d854421c5f756285f85fd091254
Kevin Hu <khu@liveperson.com>
2019-12-08 22:22:29 -0500

    aborted cache because had trouble with asynchronicity of set method; storing bearer in browser session storage instead; created models for engHist and msgInt api calls

--- server/src/services/data-history/data-history.service.ts
+++ server/src/services/data-history/data-history.service.ts
@@ -8,6 +8,7 @@ import {
 import { Observable } from "rxjs";
 import { map, delay } from "rxjs/operators";
 import { AxiosResponse } from "axios";
+import { generateHeaders } from "../../utils/generateHeaders";
 
 @Injectable()
 @CacheKey("bearer")
@@ -29,11 +30,7 @@ export class DataHistoryService {
     // end time for test - current time set to default but bring in from body as selected by user
     !to ? (to = Date.now()) : to;
     // let response = await this.cacheManager.mget(key1, key2,async (err, result) => { })
-    const headers = {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      Authorization: `Bearer ${bearer}`
-    };
+    const headers = generateHeaders(bearer);
     const payload = {
       interactive: true,
       ended: true,
@@ -58,6 +55,29 @@ export class DataHistoryService {
     accountId: string,
     body: { from?: number; to?: number }
   ): Promise<any> {
-
+    let { from, to } = body;
+    //start time for test - set to default upon load but bring in from body when selected by user
+    !from ? (from = Date.now() - 60000 * 60 * 24 * 30) : from; // 1 months of data
+    // end time for test - current time set to default but bring in from body as selected by user
+    !to ? (to = Date.now()) : to;
+    // let response = await this.cacheManager.mget(key1, key2,async (err, result) => { })
+    const headers = generateHeaders(bearer);
+    const payload = {
+      interactive: true,
+      ended: true,
+      start: {
+        from,
+        to
+      }
+    };
+    let url = `https://va.msghist.liveperson.net/messaging_history/api/account/${accountId}/conversations/search?`;
+    console.log("hit msg interaction service");
+    const msgIntHistData = await this.http
+      .post(url, payload, { headers: headers })
+      .pipe(map(response => response.data))
+      .toPromise()
+      .catch(err => err);
+    console.log(msgIntHistData);
+    return msgIntHistData;
   }
 }
